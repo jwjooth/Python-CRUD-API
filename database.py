4dcbe2784340dd2d3dcb69b622b17daeae9a5436
jwjooth <185214226+jwjooth@users.noreply.github.com>
2026-09-22 11:22:22 +0700

    test commit

--- database.py
+++ database.py
@@ -15,6 +15,8 @@ DATABASE_URL = (
     f"{settings.db_host}:{settings.db_port}/{settings.db_name}?charset=utf8mb4"
 )
 
+roger
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=settings.db_echo)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
