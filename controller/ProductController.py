5cce10559cc15c48cbcea9427366e7bd61e57295
jwjooth <185214226+jwjooth@users.noreply.github.com>
2026-09-22 13:11:10 +0700

    feat: controller/handler layer for product api

---
+++ controller/ProductController.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Query, Depends, Response, status
+from sqlalchemy.orm import Session
+
+from database import get_db
+from payload.ProductPayload import ProductResponse, ProductRequest
+from service.ProductService import ProductService
+
+router = APIRouter(prefix="api/v1/products", tags=["products"])
+
+
+@router.get("", response_model=list[ProductResponse])
+def get_all(
+        offset: int = Query(0, ge=0),
+        limit: int = Query(100, ge=1, le=100),
+        db: Session = Depends(get_db),
+):
+    service = ProductService(db)
+    return service.get_all(offset, limit)
+
+
+@router.get("/{id}", response_model=ProductResponse)
+def get_by_id(
+        id: int,
+        db: Session = Depends(get_db),
+):
+    service = ProductService(db)
+    return service.get_by_id(id)
+
+
+@router.post("", response_model=ProductResponse)
+def create(
+        request: ProductRequest,
+        db: Session = Depends(get_db)
+):
+    service = ProductService(db)
+    return service.create(request)
+
+
+@router.put("/{id}", response_model=ProductResponse)
+def update(
+        id: int,
+        request: ProductRequest,
+        db: Session = Depends(get_db),
+):
+    service = ProductService(db)
+    return service.update(id, request)
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    service = ProductService(db)
+    service.delete(id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
